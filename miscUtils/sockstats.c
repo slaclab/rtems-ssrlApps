88d69d91d5e915d1bebac59b0cfbe5e3d195f21e
strauman <>
2008-07-22 05:09:36 +0000

    - added a lot more info to sockstats (internal BSD socket info such as mbuf    space held etc.).

--- miscUtils/sockstats.c
+++ miscUtils/sockstats.c
@@ -1,5 +1,6 @@
 #ifdef __rtems__
 #include <rtems.h>
+#include <rtems/rtems_bsdnet_internal.h>
 #include <rtems/libio_.h>
 #else
 #define rtems_libio_number_iops 2000
@@ -10,18 +11,138 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+#ifdef __rtems__
+#include <sys/socketvar.h>
+#include <sys/protosw.h>
+#include <sys/mbuf.h>
+#include <net/if.h>
+#include <net/if_var.h>
+#endif
+
 #if ! defined(__rtems__) 
-#define LOCK() do {} while (0)
+
+#define LOCK()   do {} while (0)
 #define UNLOCK() do {} while (0)
-#else
-#define UNLOCK() do { rtems_semaphore_release( rtems_libio_semaphore ); } while (0)
-#define LOCK()   do { rtems_semaphore_obtain( rtems_libio_semaphore, RTEMS_WAIT, RTEMS_NO_TIMEOUT ); } while (0)
+
+#else /* __rtems__ */
+
+/* acquire locks in the same order RTEMS syscalls do! */
+static inline void LOCK(void)
+{
+	rtems_bsdnet_semaphore_obtain();
+	rtems_semaphore_obtain( rtems_libio_semaphore, RTEMS_WAIT, RTEMS_NO_TIMEOUT );
+}
+
+static inline void UNLOCK(void)
+{
+	rtems_semaphore_release( rtems_libio_semaphore );
+	rtems_bsdnet_semaphore_release();
+}
+
+/*
+ * Extra socket stats from the guts of RTEMS.
+ * 'sbstats' caches the info from the so_rcv
+ * and so_snd members of 'struct socket'.
+ */
+struct sbstats {
+		u_int sb_cc;     /* chars in buffer                  */
+		u_int sb_hiwat;  /* max char count                   */
+		u_int sb_mbcnt;  /* chars of mbufs used              */
+		u_int sb_mbmax;  /* max chars of mbufs to use        */
+		u_int sb_lowat;  /* low-water mark                   */
+		u_int nmbufs;    /* number of mbufs in chain         */
+		short sb_flags;  /* flags                            */
+		int   sb_timeo;  /* timeout for read/write           */
+};
+
+/*
+ * Extra socket stats from the guts of RTEMS.
+ * 'sostats' caches the info from 'struct socket'.
+ */
+struct sostats {
+	short	so_type;
+	short   so_options;
+	/* INET domain was already checked by getsockname        */
+	short   pr_protocol;
+	short   so_qlen;    /* unaccepted connections            */
+	short   so_incqlen; /* unaccepted incomplete connections */
+	short   so_qlimit;  /* max. number of queued connections */
+	short   so_timeo;   /* timeout                           */
+	struct sbstats so_rcv, so_snd;
+};
+
+static void
+getsbstats(struct sbstats *sbs, struct sockbuf *sob)
+{
+struct mbuf *mb, *mbp;
+u_int        k;
+
+	sbs->sb_cc    = sob->sb_cc;
+	sbs->sb_hiwat = sob->sb_hiwat;
+	sbs->sb_mbcnt = sob->sb_mbcnt;
+	sbs->sb_mbmax = sob->sb_mbmax;
+	sbs->sb_lowat = sob->sb_lowat;
+	/* count mbufs */
+	for ( k=0, mbp=sob->sb_mb; mbp; mbp=mbp->m_nextpkt ) {
+		for ( mb = mbp; mb; mb=mb->m_next ) {
+			k++;
+		}
+	}
+	sbs->nmbufs   = k;
+	sbs->sb_flags = sob->sb_flags;
+	sbs->sb_timeo = sob->sb_timeo;
+}
+
+/*
+ * Helper to print the members of 'struct sbstats'
+ *
+ * RETURNS: Number or chars printed.
+ */
+static int
+prsbstats(FILE *f, struct sbstats *sb, int level)
+{
+int rval = 0;
+
+	rval += fprintf(f, "     Number of mbufs in chain    : %u\n",      sb->nmbufs);
+	rval += fprintf(f, "     Chars of mbufs used         : %u\n",      sb->sb_mbcnt);
+
+	if ( level > 1 ) {
+		rval += fprintf(f, "     Chars in buffer             : %u\n",      sb->sb_cc);
+		rval += fprintf(f, "     Max char count (hiwat)      : %u\n",      sb->sb_hiwat);
+		rval += fprintf(f, "     Max chars of mbufs to use   : %u\n",      sb->sb_mbmax);
+		rval += fprintf(f, "     Low-water mark              : %u\n",      sb->sb_lowat);
+		rval += fprintf(f, "     Flags                       : 0x%04hx\n", sb->sb_flags);
+		rval += fprintf(f, "     Timeout for read/write      : %i\n",      sb->sb_timeo);
+	}
+	return rval;
+}
+
+/*
+ * Print a string 'str' using 'fmt'. If 'str' is NULL then
+ * print the numerical value of 'num' instead (but using a
+ * different format, namely '%i').
+ *
+ * RETURNS: Number of chars printed.
+ */
+static int
+prstrnum(FILE *f, const char *fmt, const char *str, int num)
+{
+int rval;
+
+	rval = fprintf(f, fmt, str ? str : "");
+	if ( !str ) {
+		rval += fprintf(f, "%i", num);
+	}
+
+	return rval;
+}
+
 #endif
 
 int
-sockstats(FILE *f)
+rtems_bsdnet_show_socket_stats(FILE *f, int level, int sd)
 {
-int             i,j,e;
+int             i,j,e,min,max;
 
 union {
 struct sockaddr sa;
@@ -30,10 +151,40 @@ struct sockaddr_in sin;
 socklen_t       l;
 char            buf[100];
 struct stat     sb;
+#ifdef __rtems__
+/* grab a copy of some socket statistics */
+struct sostats  sostats;
+struct socket   *so      = 0;
+rtems_libio_t   *iop     = 0;
+#endif
 
 	if ( ! f )
 		f = stdout;
-	for ( i=j=0; i< rtems_libio_number_iops; i++ ) {
+
+	/* Since file-descriptor zero is usually the (serial) console
+	 * it will rarely be a socket. For convenience we use 'sd==0'
+	 * as an indicator that they want info about all sockets.
+	 * If there is one day a socket in the first slot they just
+	 * can't look *only* at that one, sorry.
+	 */
+
+	/*
+	 * Maybe we change the semantics one day so that sd<0 means 'all'...
+	 */
+	if ( sd < 0 )
+		sd = 0;
+
+	if ( sd ) {
+		if ( sd >= rtems_libio_number_iops ) {
+			/* invalid argument */
+			return -1;
+		}
+		min = sd; max = sd+1;
+	} else {
+		min = 0;  max = rtems_libio_number_iops;
+	}
+
+	for ( i=min, j=0; i<max; i++ ) {
 		l = sizeof(ss);
 		LOCK();
 		if ( fstat(i, &sb) || ! S_ISSOCK(sb.st_mode) || getsockname(i, &ss.sa, &l) ) {
@@ -46,6 +197,28 @@ struct stat     sb;
 		} else {
 			l = sizeof(sp);
 			e = getpeername(i, &sp.sa, &l);
+
+#ifdef __rtems__
+			/* Gather some statistics from the socket */
+			if ( level && (iop = rtems_libio_iop(i)) ) {
+				if ( (so = (struct socket*)iop->data1) ) {
+
+					sostats.so_type      = so->so_type;
+					sostats.so_options   = so->so_options;
+					sostats.pr_protocol  = so->so_proto->pr_protocol;
+					sostats.so_qlen      = so->so_qlen;
+					sostats.so_incqlen   = so->so_incqlen;
+					sostats.so_qlimit    = so->so_qlimit;
+					sostats.so_timeo     = so->so_timeo;
+
+					/* rx buffer */
+					getsbstats(&sostats.so_rcv, &so->so_rcv);
+
+					/* tx buffer */
+					getsbstats(&sostats.so_snd, &so->so_snd);
+				}
+			}
+#endif
 			UNLOCK();
 
 			j++;
@@ -59,7 +232,49 @@ struct stat     sb;
 				inet_ntop(AF_INET, &sp.sin.sin_addr, buf, sizeof(buf));
 				fprintf(f, "%16s:%5u\n", buf, ntohs(sp.sin.sin_port));
 			}
+
+#ifdef __rtems__
+			if ( level ) {
+				/* print more information */
+				if ( !iop ) {
+					fprintf(f,"Unable to find socket IOP (why ?)\n");
+				} else if ( ! so ) {
+					fprintf(f,"Unable to find socket data (why ?)\n");
+				} else {
+					/* Everything found OK; print info */
+					const char     *str;
+
+					switch ( sostats.so_type ) {
+						case SOCK_STREAM: str = "SOCK_STREAM"; break;
+						case SOCK_DGRAM : str = "SOCK_DGRAM";  break;
+						case SOCK_RAW   : str = "SOCK_RAW";    break;
+						default:          str = 0;             break;
+					}
+					prstrnum(f,"   Type: %11s", str, sostats.so_type);
+					fprintf(f,"; Options: 0x%04x;", sostats.so_options);
+					switch ( sostats.pr_protocol ) {
+						case IPPROTO_ICMP: str = "ICMP"; break;
+						case IPPROTO_TCP : str = "TCP";  break;
+						case IPPROTO_UDP : str = "UDP";  break;
+						default:           str = 0;      break;
+					}
+					prstrnum(f, "   Protocol: %4s", str, sostats.pr_protocol);
+					fprintf(f, "\n");
+					if ( level > 1 ) {
+						fprintf(f, "   Unaccepted connections           : %hi\n", sostats.so_qlen);
+						fprintf(f, "   Unaccepted incomplete connections: %hi\n", sostats.so_incqlen);
+						fprintf(f, "   Max. number of queued connection : %hi\n", sostats.so_qlimit);
+						fprintf(f, "   Timeout                          : %hi\n", sostats.so_timeo);
+					}
+					fprintf(f, "   Receiving buffers:\n");
+					prsbstats(f, &sostats.so_rcv, level);
+					fprintf(f, "   Transmitting buffers:\n");
+					prsbstats(f, &sostats.so_snd, level);
+				}
+			}
+#endif
 		}
+		fprintf(f,"\n");
 	}
 	return j;
 }
@@ -70,10 +285,12 @@ CEXP_HELP_TAB_BEGIN(sockstats)
 	HELP(
 "Dump local and peer (if applicable) address of all AF_INET\n"
 "sockets in the system.\n"
+"If 'level' is nonzero then more (internal) information is printed\n"
+"If 'sd' is nonzero then only info about 'sd' is printed\n"
 "RETURNS: number of live (used) sockets. The max. number of\n"
 "         file descriptors can be obtained from the (READ-ONLY)\n"
 "         variable 'rtems_libio_number_iops'.\n",
-	int, sockstats,  (FILE *f)
+	int, rtems_bsdnet_show_socket_stats,  (FILE *f, int level, int sd)
 	),
 CEXP_HELP_TAB_END
 #endif
@@ -90,6 +307,6 @@ struct sockaddr    sa;
 	sa.sin.sin_port   = htons(5432);
 	sd = socket(PF_INET, SOCK_STREAM, 0);
 	bind(sd, &sa.sa, sizeof(sa));
-	sockstats(0);
+	rtems_bsdnet_show_socket_stats(0,0,0);
 }
 #endif
